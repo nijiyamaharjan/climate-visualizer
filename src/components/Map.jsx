f4dba82ff9b810f5735e2636a65d1f0efc771e25
nijiyamaharjan <nijiyamn@gmail.com>
2024-12-02 18:39:03 +0545

    added layers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,32 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, LayersControl } from 'react-leaflet';
 import React, { useState, useEffect } from 'react';
 import L, { divIcon } from "leaflet";
 import 'leaflet/dist/leaflet.css';
 
 export default function MapComponent() {
     const [districts, setDistricts] = useState(null);
+    const [provinces, setProvinces] = useState(null);
 
     useEffect(() => {
         // Import the GeoJSON file from the public folder
         fetch('/districts.geojson')
             .then(response => response.json())
             .then(data => {
-                console.log('Loaded GeoJSON data')
+                console.log('Loaded districts data')
                 setDistricts(data)
             })
-            .catch(console.log('Error loading GeoJSON'))
+            .catch(console.log('Error loading districts'))
+    }, []);
+
+    useEffect(() => {
+        // Import the GeoJSON file from the public folder
+        fetch('/provinces.geojson')
+            .then(response => response.json())
+            .then(data => {
+                console.log('Loaded provinces data')
+                setProvinces(data)
+            })
+            .catch(console.log('Error provinces GeoJSON'))
     }, []);
 
     const setColor = ({ properties }) => {
@@ -38,10 +50,6 @@ export default function MapComponent() {
         });
     };
 
-    if (!districts) {
-        return <div>Loading map...</div>;
-    }
-
     return (
         <div className="relative w-full h-screen">
             <MapContainer
@@ -53,29 +61,39 @@ export default function MapComponent() {
                 scrollWheelZoom={true}
             >
                 <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png" />
-                <GeoJSON 
-                    data={districts} 
-                    style={setColor}
-                    onEachFeature={(feature, layer) => {
-                        layer.bindPopup(`
-                            District: ${feature.properties.DISTRICT}<br>
-                            Headquarters: ${feature.properties.HQ}<br>
-                            Province: ${feature.properties.PROVINCE}
-                        `);
-                    }}
-                />
-                <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png" />
-                <GeoJSON 
-                    data={districts} 
-                    style={setColor}
-                    onEachFeature={(feature, layer) => {
-                        layer.bindPopup(`
-                            District: ${feature.properties.DISTRICT}<br>
-                            Headquarters: ${feature.properties.HQ}<br>
-                            Province: ${feature.properties.PROVINCE}
-                        `);
-                    }}
-                />
+
+                <LayersControl position='topright'>
+                    <LayersControl.Overlay checked name="Districts">
+                        {districts && (
+                            <GeoJSON 
+                                data={districts} 
+                                style={setColor}
+                                onEachFeature={(feature, layer) => {
+                                    layer.bindPopup(`
+                                        District: ${feature.properties.DISTRICT}<br>
+                                        Headquarters: ${feature.properties.HQ}<br>
+                                        Province: ${feature.properties.PROVINCE}
+                                    `);
+                                }}
+                            />
+                        )}
+                    </LayersControl.Overlay>
+
+                    <LayersControl.Overlay name='Provinces'>
+                        {provinces && (
+                            <GeoJSON 
+                                data={provinces} 
+                                style={setColor}
+                                onEachFeature={(feature, layer) => {
+                                    layer.bindPopup(`
+                                        Name: ${feature.properties.name}<br>
+                                        Capital: ${feature.properties.capital}<br>
+                                    `);
+                                }}
+                            />
+                        )}
+                    </LayersControl.Overlay>
+                </LayersControl>              
             </MapContainer>
         </div>
     );
